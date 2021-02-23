98b9fe7eb1119a5bf4557e6d68e31785c3589076
Thiago Raniery Brito <thiagobritotrs@gmail.com>
2021-02-23 18:46:33 -0300

    Add user model

---
+++ api/src/models/User.ts
@@ -0,0 +1,25 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
+import { v4 as uuid } from "uuid";
+
+@Entity("users") // informamos ao typeorm que a classe User é equivalente à entidade/tabela "users" no banco de dados.
+class User {
+    @PrimaryColumn()
+    readonly id: string;
+
+    @Column()
+    name: string; // podemos mudar, por questões de ligibilidade de código, o nome das colunas no model, contudo, quando o fazemos, é necessário informa o nome da coluna na anotation.
+
+    @Column()
+    email: string;
+
+    @CreateDateColumn()
+    created_at: Date
+
+    constructor() {
+        if (!this.id) {
+            this.id = uuid();
+        }
+    }
+}
+
+export { User };
