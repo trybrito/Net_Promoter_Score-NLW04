48c9663ee9a2bf9defd39b2e8106ef6845cc7792
Thiago Raniery Brito <thiagobritotrs@gmail.com>
2021-02-23 18:45:24 -0300

    Update server file

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,28 +1,11 @@
+import 'reflect-metadata' // o reflect-metadata tem de ser a primeria importação.
 import express from "express";
+import "./database"; // por padrão a aplicação já reconhece que, se dentro de database existir um arquivo nomeado de index, é este que ela executará. 
+import { router } from './routes';
 
 const app = express();
 
-/** Métodos HTTP -> mais comuns.
- * GET -> Busca
- * POST -> Salvar
- * PUT -> Alterar
- * DELETE -> Deletar
- * PATCH -> Alteração específica
-*/
-
-// http://localhost:3333/ -> URI de acesso no presente caso.
-
-app.get("/", (request, response) => {
-    return response.json({ message: "Hello World - NLW04" });
-});
-
-/** Parâmetros dos métodos HTTP
- * 1° parâmetro: Rota (Recurso da API)
- * 2° parâmetro: request (requisição),response (resposta)
-*/
-app.post('/', (request, response) => {
-    // Recebeu os dados para salvar
-    return response.json({ message: "Os dados foram gravados com sucesso!" });
-});
+app.use(express.json()); // informa ao express que utilizaremos json nos requests do arquivo de rotas.
+app.use(router); // o use, nesse caso, faz um papel parecido com o de um middleware.
 
 app.listen(3333, () => console.log("Server is running!"));
