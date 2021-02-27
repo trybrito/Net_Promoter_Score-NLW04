2a9e0b9c807ddeb83b046bd7b4cd49ea4d3d1d5c
Thiago Raniery Brito <thiagobritotrs@gmail.com>
2021-02-26 22:00:28 -0300

    Update test file

--- api/src/__tests__/User.test.ts
+++ api/src/__tests__/User.test.ts
@@ -1,4 +1,5 @@
 import request from "supertest";
+import { getConnection } from "typeorm";
 import { app } from "../app";
 import createConnection from "../database";
 
@@ -7,6 +8,12 @@ describe("Users", () => {
         const connection = await createConnection();
         await connection.runMigrations();
     });
+
+    afterAll(async () => {
+        const connection = getConnection();
+        await connection.dropDatabase();
+        await connection.close();
+    }); // encerra a conexão e exclui o bd de testes.
 
     it("Should be able to create a new user", async () => {
         const response = await request(app).post("/users").send({
